7100d0dc03caefb509ee191dfdd166931c971cea
Or Hasson <hassonor@gmail.com>
2023-10-01 12:38:00 +0300

    Initial commit

---
+++ project_1/frontend/src/screens/Home.js
@@ -0,0 +1,77 @@
+import React, {useState} from 'react';
+import './styles/style.css';
+
+function Home (){
+    const [inputValue, setInputValue] = useState('');
+    const [error, setError] = useState('');
+    const [result, setResult] = useState('');
+    const [prompt, setPrompt] = useState('');
+    const [jresult, setJresult] = useState('');
+
+    const handleSubmit = async(event) => {
+        event.preventDefault();
+        if(!inputValue){
+            setError('Please enter a prompt!');
+            setPrompt('');
+            setResult('');
+            setJresult('');
+            return;
+        }
+        try{
+            const response = await fetch('/api/chatgpt', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    text: inputValue
+                })
+            });
+
+            if(response.ok){
+                const data = await response.json();
+                console.log(error);
+            }
+            else{
+                throw new Error('An error occurred while submitting the form.');
+            }
+
+        }
+        catch(error){
+            console.log(error);
+            setResult('');
+            setError('An error occurred while submitting the form.');
+            setResult('');
+        }
+    }
+
+    return (
+        <div className='container'>
+            <form className='form-horizontal' onSubmit={handleSubmit}>
+            <div className='row form-group  mt-2'>
+                <div className='col-sm-10'>
+                    <div className='form-floating'>
+                        <textarea
+                            className='form-control custom-input'
+                            id='floatingTextarea'
+                            placeholder='Enter a prompt'
+                            style={{height: '100px'}}
+                            value={inputValue}
+                            onChange={(event) => setInputValue(event.target.value)}
+                        />
+                        <label htmlFor='floatingTextarea'>Input</label>
+                    </div>
+                </div>
+                <div className='col-sm-2'>
+                    <button type="submit" className='btn btn-primary custom-button'>Submit</button>
+                </div>
+            </div>
+            </form>
+            {error && <div className='alert alert-dark mt-3'>{error}</div>}
+            {prompt && <div className='alert alert-secondary mt-3'>{prompt}</div>}
+            {result && <div className='alert alert-success mt-3'>{result}</div>}
+            {result && <pre className='alert alert-info mt-3'><code>{jresult}</code></pre>}
+        </div>
+    )
+
+}
+
+export default Home;
